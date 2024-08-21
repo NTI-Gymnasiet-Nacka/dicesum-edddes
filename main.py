c1520a0f24281e5a22d7f078619f2c9db9e7bbfc
edddes <edde.ranheden@gmail.com>
2024-08-21 11:11:24 +0200

    dice (ej klar)

--- main.py
+++ main.py
@@ -2,8 +2,27 @@
 # Författare: 
 # Datum:
 
+import os
+
 def main():
     user_input = input().split(" ")
 
 if __name__ == "__main__":
     main()
+
+
+
+# först frågar programmet hur många sidor tärningarna har
+dice_sides = int(input("Skriv in hur många sidor tärningen har: "))
+
+for dice1 in range (1, dice_sides + 1):
+    for dice2 in range (1, dice_sides + 1):
+        sum_dice = dice1 + dice2
+
+sum_dice 
+
+
+
+
+
+#använd most common
